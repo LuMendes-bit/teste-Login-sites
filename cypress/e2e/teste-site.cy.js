f470d305a1b4371719ffae83e424570783c1e6dd
Lucas Mendes <lucas.mendes.1234@hotmail.com>
2025-09-04 21:14:56 -0300

    att sucesso e falha

--- cypress/e2e/teste-site.cy.js
+++ cypress/e2e/teste-site.cy.js
@@ -10,4 +10,16 @@ describe('Loja de produtos', () => {
     cy.get('#nameofuser').should('contain','Welcome luTesteSenac');
   });
 
+
+  it('Deve falhar o login', () => {
+    cy.visit('https://www.demoblaze.com/');
+    cy.get("#login2").click();
+    cy.get('#loginusername').type('luTesteSenac');
+    cy.get('#loginusername').clear();
+    cy.get('#loginusername').type('luTesteSenac');
+    cy.get('#loginpassword').type('wrhgwhgju');
+    cy.get('button[onclick="logIn()"]').click();
+    cy.on('window:alert', (text) => {expect(text).to.eq('Wrong password.');});
+    
+  });
 });
